11039fe257ded0370288dae35ac276ea5a90a90c
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2022-08-18 16:48:42 +0600

    cors added

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemRouter = require("./routes/itemRoutes");
+var cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9pclo.mongodb.net/mptfilTask?retryWrites=true&w=majority`;
