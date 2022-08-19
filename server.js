f08675fb4730ed593a14b4af8c9e11bc38e4453a
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2022-08-19 08:37:17 +0600

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemRouter = require("./routes/itemRoutes");
-var cors = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
